5d1b1bb677e702182d4860ee7a560c013e055822
Mert Sakınç <mertsak2@gmail.com>
2024-03-14 12:48:43 +0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://vocal-mantis-20.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "../style/globals.css";
+import ConvexClientProvider from "./ConvexClientProvider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
